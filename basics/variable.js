991ad9c54a8908421b31d29e7efa86747891a628
MD TAUSIF ALAM <91112604+mdtausif281@users.noreply.github.com>
2025-09-08 17:09:20 +0000

    configuration

---
+++ basics/variable.js
@@ -0,0 +1,16 @@
+const accountId =144553
+let accountEmail ="mdtausifalam2311@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState
+
+// accountId=2(not allowed)
+console.log(accountId);
+accountEmail="hc@gmail.com"
+accountPassword="7666"
+accountCity="kolkata"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
